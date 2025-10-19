9acc5478320a0dfb733e6c0a00da85d4ab1fdb8c
Juan <juanmetchart@gmail.com>
2025-10-18 23:41:58 -0300

    Remove startLocation field

--- src/types/race.types.ts
+++ src/types/race.types.ts
@@ -17,7 +17,6 @@ export interface CreateRaceDto {
   country: string;
   location: string;
   website: string;
-  startLocation: string;
   raceType: RaceType;
   raceDistances: CreateRaceDistance[];
 }
